a08e9e29d55344e7cbc85c24fa970138024a4ea9
zoe5466 <32200033+zoe5466@users.noreply.github.com>
2025-01-24 10:43:31 +0800

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDvnb7zYxmaT6bE44PpHpkx5eSL5vfQTyc",
+  authDomain: "upsaceevent.firebaseapp.com",
+  databaseURL: "https://upsaceevent-default-rtdb.firebaseio.com",
+  projectId: "upsaceevent",
+  storageBucket: "upsaceevent.firebasestorage.app",
+  messagingSenderId: "927813294597",
+  appId: "1:927813294597:web:7b868bee2602a9d92b1a6c",
+  measurementId: "G-LFGRN0KV0G"
+};
