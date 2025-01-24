9cd96f1e890cf99e963f0af7e4328cccff6e55f2
zoe5466 <32200033+zoe5466@users.noreply.github.com>
2025-01-24 11:05:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // 引入 Firebase SDK
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
 // 初始化 Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// 連接到 Firestore
-const db = firebase.firestore();
-const auth = firebase.auth();
+const firebaseConfig = {
+  apiKey: "AIzaSyDvnb7zYxmaT6bE44PpHpkx5eSL5vfQTyc",
+  authDomain: "upsaceevent.firebaseapp.com",
+  databaseURL: "https://upsaceevent-default-rtdb.firebaseio.com",
+  projectId: "upsaceevent",
+  storageBucket: "upsaceevent.firebasestorage.app",
+  messagingSenderId: "927813294597",
+  appId: "1:927813294597:web:7b868bee2602a9d92b1a6c",
+  measurementId: "G-LFGRN0KV0G"
+};
 
 // 組別與座位圖
 const groups = [
